43ef6154a2cbaaf44781f4f595ae42beeec7e3f2
Curtis Nix <curtis.nix@proton.me>
2023-06-02 10:15:57 -0400

    Added image and current position

--- js/script.js
+++
@@ -1,3 +0,0 @@
-const api_url = 'https://zmnzu13dm1.execute-api.us-east-1.amazonaws.com/default/crcTestingFunction'
-been_viewed = fetch(api_url) 
-document.body.innerHTML = "<h1>This has been viewed " + been_viewed + "</h1>"
